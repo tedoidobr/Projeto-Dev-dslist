e4bd893db2e6640d09b4a815384c89fab291ff94
tedoidobr <tedoido@gmail.com>
2023-11-19 15:23:58 -0300

    project Created

---
+++ src/main/java/com/daniel/dlsist/DlsistApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.dlsist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlsistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlsistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/dlsist/DlsistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.dlsist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlsistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
