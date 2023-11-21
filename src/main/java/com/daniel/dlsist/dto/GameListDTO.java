b5c3bf445481ef81646b1f66849fef0e516fe7bc
tedoidobr <tedoido@gmail.com>
2023-11-20 22:35:33 -0300

    Get list

---
+++ src/main/java/com/daniel/dlsist/dto/GameListDTO.java
@@ -0,0 +1,27 @@
+package com.daniel.dlsist.dto;
+
+import com.daniel.dlsist.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name =entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+}
---
+++ src/main/java/com/daniel/dlsist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.daniel.dlsist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.daniel.dlsist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
