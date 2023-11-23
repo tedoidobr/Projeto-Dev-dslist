0f2c43c55ba2e92b7dcc8a42bc75c973cac8eef1
tedoidobr <tedoido@gmail.com>
2023-11-23 19:24:36 -0300

    Revisional

--- src/main/java/com/daniel/dlsist/controllers/GameListController.java
+++ src/main/java/com/daniel/dlsist/controllers/GameListController.java
@@ -33,7 +33,7 @@ public class GameListController {
 
 	}
 
-	@GetMapping(value = "/{listId}games/")
+	@GetMapping(value = "/{listId}/games")
 	public List<GameMinDTO> findByList(@PathVariable Long listId) {
 		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
